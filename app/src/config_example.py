3c5f7bb167d81dcdfd501b15519ad7ef2608654e
Jiri Hubacek <hubacekjirka@gmail.com>
2020-01-12 12:15:51 +1100

    config example update

--- app/src/config_example.py
+++ app/src/config_example.py
@@ -7,6 +7,17 @@ consumer_secret = 'CONSUMERSECRET'
 access_token = 'ACCESS-TOKEN'
 access_token_secret = 'ACCESSTOKENSECRET'
 
-telegram_token = 'TELEGRAMBOTTOKEN'
+# Folder for storing Telegram's ChatIds
+chatIdFolder = os.path.join(os.path.dirname(os.path.realpath(__file__)),"photos")
+telegram_token = 'TELEGRAM TOKEN'
 
+# values 'S3', anything else defaults to using the local folder only
+photoSource = 'S3'
+awsAccessKey = 'AWSACCESSKEY'
+awsKeyId = 'AWSKEYID'
+awsBucket = 'AWSBUCKETNAME'
+
+# debugging variables
+tweetingEnabled = True
+telegramingEnabled = True
 debug = True
